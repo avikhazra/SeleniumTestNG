00dbf4cf9676c07493e18ab10af505a08a825ee8
Avik Hazra <hazraavik11@gmail.com>
2020-07-23 21:50:33 +0530

    update
    update

--- src/main/java/pageObjects/ProductDetailPage.java
+++ src/main/java/pageObjects/ProductDetailPage.java
@@ -4,6 +4,7 @@ package pageObjects;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
@@ -14,7 +15,7 @@ public class ProductDetailPage {
 private static final Logger log = Logger.getLogger(ProductDetailPage.class);
 	
 	//ProductDetail page constructor
-@FindBy(xpath = "(//a[contains(@class,'products-block-image content_img')]//img)[1]")
+@FindBy(xpath = "((//a[contains(@class,'products-block-image')]/descendant::img[1])[1]")
 private WebElement Select1StProduct;
 
 @FindBy(xpath = "//button[@name='Submit']//span[1]")
@@ -26,13 +27,18 @@ private List<WebElement> proceedToCheckoutBox;
 @FindBy(xpath = "//a[contains(@class,'button btn')]//span[1]")
 private List<WebElement> proceedToCheckoutButton;
 
+
 	public ProductDetailPage() {
 		super();
 		log.info("ProductDetailPage constructor is invoked");
+		
 	}
 	
 	//Product add Method  by Avik
 	public void Select1stProdduct() throws InterruptedException {
+		
+		
+		
 		BrowserUtil.click(Select1StProduct);
 		BrowserUtil.click(BrowserUtil.ElementWaitToClickable(Addtocart));
 		BrowserUtil.PageReadyStateCheck(4000);
